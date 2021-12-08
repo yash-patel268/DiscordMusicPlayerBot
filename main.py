771e52c9e4c6b15ec48ff0c9f93b09ded69761ab
Yash Patel <84209710+yash-patel268@users.noreply.github.com>
2021-12-08 00:26:12 +0000

    Added music to main

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
 import discord
 from discord import commands
+import music
+
+cogs = [music]
+
+for i in range(len(cogs)):
+  cogs[i].setup()
 
 client = commands.Bot(command_prefix="?", intents= discord.Intents.all())
 
