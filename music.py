6710da44f7c4cb6e819c0fb0d9b70020d24c3dd1
Yash Patel <84209710+yash-patel268@users.noreply.github.com>
2021-12-08 00:24:28 +0000

    Added cogs

--- music.py
+++ music.py
@@ -2,3 +2,9 @@ import discord
 from discord.ext import commands
 import youtube_dl
 
+class music(commands.Cog):
+  def __init__(self, client):
+    self.client = client
+
+def setup(client):
+  client.add_cog(music(client))
